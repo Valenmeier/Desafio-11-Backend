5dae407c642f8f314f95dbae45f2d76b3cb36765
Valentin Meier <valentinmeier123@gmail.com>
2023-02-23 22:04:58 -0300

    arreglando creación de usuario

--- src/components/users/usersModel.js
+++ src/components/users/usersModel.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { userSchema } from "./usersSchema.js";
+import { CartController } from "../carts/cartController.js";
+
 const userCollection = "users";
 
 const userModel = mongoose.model(userCollection, userSchema);
@@ -8,13 +10,16 @@ export class UsersModel {
   constructor() {
     this.db = userModel;
   }
-  createUser=async(user)=>{
-    return await this.db.create(user)
-  }
-  searchUser=async(email)=>{
-    return await this.db.findOne({ email:email });
-  }
-  searchUserById=async(id)=>{
-    return await this.db.findById(id)
-  }
+  createUser = async (user) => {
+    let cartController = new CartController();
+    let cart = await cartController.createCart();
+    user.cart = cart._id;
+    return await this.db.create(user);
+  };
+  searchUser = async (email) => {
+    return await this.db.findOne({ email: email });
+  };
+  searchUserById = async (id) => {
+    return await this.db.findById(id);
+  };
 }
